d29705ab1d892213fd2be6190cb51e522a5f754d
parroit <andrea.parodi@ebansoftware.net>
2013-10-12 18:30:13 +0200

    changed configuration

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -5,34 +5,7 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
 
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            lib: {
-                src: ['lib/**/*.js']
-            },
-            test: {
-                src: ['test/**/*.js']
-            }
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib: {
-                files: '<%= jshint.lib.src %>',
-                tasks: ['jshint:lib', 'nodeunit']
-            },
-            test: {
-                files: '<%= jshint.test.src %>',
-                tasks: ['jshint:test', 'nodeunit']
-            }
-        },
+
         mochaTest: {
             test: {
                 options: {
@@ -44,13 +17,10 @@ module.exports = function (grunt) {
     });
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-mocha-test');
 
-    grunt.registerTask('default', 'mochaTest');
+    grunt.registerTask('test', 'mochaTest');
 
-    // Default task.
-    grunt.registerTask('deploy', ['mochaTest','jshint']);
+
 
 };
